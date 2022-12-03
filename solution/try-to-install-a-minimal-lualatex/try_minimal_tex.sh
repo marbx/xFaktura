3238825d58dfddcd50d5b8020dfaa57f12dfdb56
Markus Kramer <markuskramerIgitt@gmail.com>
2022-12-03 10:58:59 +0100

    try-to-install-a-minimal-lualatex

---
+++ solution/try-to-install-a-minimal-lualatex/try_minimal_tex.sh
@@ -0,0 +1,10 @@
+/usr/local/texlive/2022/install-tl -no-doc-install -no-src-install -texdir /home/markus/tmp/minimal_tex --no-interaction 
+
+/home/markus/tmp/minimal_tex/bin/x86_64-linux/tlmgr install soulutf8 soul xcolor luatexbase luacode fontspec tex-gyre german babel-german hyphen-german
+
+### DOCS
+# https://tug.org/texlive/quickinstall.html
+# https://www.tug.org/texlive/doc/install-tl.html
+
+#     wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz 
+#     tar zxvf install-tl-unx.tar.gz
