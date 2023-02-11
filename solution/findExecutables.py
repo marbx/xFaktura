d5de7ecd57dcb2d8b656d2b5b665331cf8d82c8f
Markus Kramer <markuskramerIgitt@gmail.com>
2023-02-11 20:37:42 +0100

    findExecutables returns paths

--- solution/findExecutables.py
+++ solution/findExecutables.py
@@ -1,18 +1,19 @@
-import platform
-import shutil
 import glob
 import os
+import platform
+import shutil
+import sys
 
+operatingSystem = platform.system()
 
 def findExecutables():
-    operatingSystem = platform.system()
     def couldbe(fpath):
         if os.path.isfile(fpath):
             return fpath
         else:
             return None
 
-    if operatingSystem == 'Linux':
+    if operatingSystem == 'Linux' or operatingSystem == 'Darwin':
         lualatex = shutil.which('lualatex')
         dvipdfmx = shutil.which('dvipdfmx')
         # Livetex may not install to path, therefore search at known locations
@@ -22,22 +23,23 @@ def findExecutables():
         if dvipdfmx is None:
             for file in glob.iglob('/usr/local/texlive/**/bin/**/dvipdfmx', recursive=True):
                 dvipdfmx = file
-        if lualatex is None or dvipdfmx is None:
-            print('ERROR    no lualatex or dvipdfmx')
-    elif operatingSystem == 'Darwin':
-        # Livetex does not install to path, therefore search at known location
-        lualatex = couldbe('/Library/TeX/texbin/lualatex')
-        dvipdfmx = couldbe('/Library/TeX/texbin/dvipdfmx')
-        # TODO /usr/local/texlive/2022/bin/universal-darwin
+        if operatingSystem == 'Darwin':
+            # Livetex does not install to path, therefore search at known location
+            lualatex = couldbe('/Library/TeX/texbin/lualatex')
+            dvipdfmx = couldbe('/Library/TeX/texbin/dvipdfmx')
+            # TODO /usr/local/texlive/2022/bin/universal-darwin
     else:
         print('ERROR    system not covered')
-
-    if not os.path.isfile(lualatex): print('ERROR    no lualatex')
-    if not os.path.isfile(dvipdfmx): print('ERROR    no dvipdfmx')
-    print( f'system     {operatingSystem}')
-    print( f'lualatex   {lualatex}')
-    print( f'dvipdfmx   {dvipdfmx}')
+        sys.exit(1)
+    return lualatex, dvipdfmx
 
 
+print( f'system     {operatingSystem}')
 
-findExecutables()
+lualatex, dvipdfmx = findExecutables()
+if lualatex is None or dvipdfmx is None:
+    print('ERROR lualatex missing')
+    sys.exit(2)
+
+print( f'lualatex   {lualatex}')
+print( f'dvipdfmx   {dvipdfmx}')
