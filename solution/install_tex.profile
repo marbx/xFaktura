8a5adabd5d67bdbdcfc5ba0d3a8c1755ca46e216
Markus Kramer <markuskramerIgitt@gmail.com>
2023-06-17 23:07:34 +0200

    adds tex to the path

---
+++ solution/install_tex.profile
@@ -0,0 +1,5 @@
+## Profile options
+
+# Adjust PATH environment variable.
+# default 0 on Unix, 1 on Windows
+instopt_adjustpath 1
--- solution/install_tex.sh
+++ solution/install_tex.sh
@@ -7,7 +7,7 @@ if command -v lualatex &> /dev/null
 then
   echo found lualatex `lualatex --version | head -n 1`
   file -b `which lualatex`
-  exit
+  #exit
 fi
 
 
@@ -16,12 +16,9 @@ then
     echo See `tlmgr list --only-installed | wc -l` TeXLive packages with tlmgr list --only-installed
 else
     echo TeXLive Manager "tlmgr" not found
-    echo THIS SCRIPT WILL NOT TRY TO INSTALL tlmgr THE TEX LIVE MANAGER
-    echo IN tlmgr, PRESS s=Schema, THEN PRESS d=basic !!!!
+    echo This script will now download and install tlmgr, the tex live manager
 fi
 
-#echo exiting now, TODO when to install what?
-#exit
 
 
 direxists() {
@@ -29,59 +26,52 @@ direxists() {
 }
 
 
-if ! [[ -f install-tl-unx.tar.gz ]] && ! [[ -f install-tl-unx.tar ]]; then
+if ! [[ -f install-tl-unx.tar.gz ]]; then
     echo /// downloading installer...
     curl -O --location  https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 fi
 
 
-if [[ -f install-tl-unx.tar.gz ]] && ! [[ -f install-tl-unx.tar ]]; then
+if [[ -f install-tl-unx.tar.gz ]]; then
     echo /// unzipping installer...
-    gunzip install-tl-unx.tar.gz
+    gunzip --force --keep install-tl-unx.tar.gz
 fi
 
 
-if ! direxists install-tl-20??????  &&  [[ -f install-tl-unx.tar ]]; then
-    echo /// untaring installer...
-    tar xf install-tl-unx.tar
-fi
+echo /// untaring installer...
+tar xf install-tl-unx.tar
+
+
+# option --no-installation
+INSTALLSTRING='sudo install-tl-20*/install-tl  --scheme=basic --no-doc-install --no-src-install --profile install_tex.profile'
+
 
 # DOCU https://tug.org/texlive/quickinstall.html
 # Installtion macht web-download egal ob schon alles da!
-echo /// installingTexLive...
-echo /// installingTexLive...
-echo /// installingTexLive...
-echo /// installingTexLive...
-echo /// installingTexLive...
-echo /// installingTexLive...
-#sudo install-tl-20*/install-tl --scheme=basic --no-doc-install --no-src-install
+echo /// installing basic TexLive...
+echo $INSTALLSTRING
+#$INSTALLSTRING
+
+echo /// Checking tlmgr version...
+tlmgr version
+
+#echo exiting now
+#exit
+
 
 #
-# /usr/local/texlive/YYYY/bin/PLATFORM  in the Pfad !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# I have to select Option O / L  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# TODO do not require user input
+#DOC https://www.tug.org/texlive/doc/install-tl.html
+## evtl nur Heros https://www.gust.org.pl/projects/e-foundry/tex-gyre
+echo /// Calling the TeX Live Manager...
+TLMGRSTRING='sudo tlmgr install soul xcolor luatexbase luacode fontspec tex-gyre german babel-german hyphen-german'
+echo $TLMGRSTRING
+$TLMGRSTRING
 
-InstallTlmgrPackages() {
-    #TODO HACK
-    #SOLUTION instopt_adjustpath (default 0 on Unix, 1 on Windows)     Adjust PATH environment variable.
-    #DOC https://www.tug.org/texlive/doc/install-tl.html
-    MARKUS=/usr/local/texlive/2023/bin/universal-darwin/tlmgr
-    ## evtl nur Heros https://www.gust.org.pl/projects/e-foundry/tex-gyre
-    echo $MARKUS install soulutf8 soul xcolor luatexbase luacode fontspec tex-gyre german babel-german hyphen-german
-    sudo $MARKUS install soulutf8 soul xcolor luatexbase luacode fontspec tex-gyre german babel-german hyphen-german
-}
 
-#TODOsudo: tlmgr: command not found
+# ERROR tlmgr install: package soulutf8 not present in repository.
+# FIX   soulutf8 --> soul
+
 #TODO ! LaTeX Error: File `luacode.sty' not found.
-
-echo /// installing required packages...
-echo /// installing required packages...
-echo /// installing required packages...
-echo /// installing required packages...
-echo /// installing required packages...
-echo /// installing required packages...
-echo /// installing required packages...
-InstallTlmgrPackages
 
 
 #Schwallereinachinstallation
@@ -92,5 +82,3 @@ InstallTlmgrPackages
 
 
 
-echo Adding texlive to the command PATH !!!!
-export PATH=/usr/local/texlive/2022/bin/x86_64-linux:$PATH
